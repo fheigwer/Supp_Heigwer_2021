79c99085c7b45b8e276599cd09601239bfcbc222
Florian Heigwer <f.heigwer@dkfz.de>
2022-07-14 11:26:59 +0200

    submission update
    submission update

---
+++ ENA-scRNA-reads/combine_lanes_into_one_file.sh
@@ -0,0 +1,23 @@
+nohup cat `find . -name Tx16-1-*R1*fastq.gz` > temp/Tx16-1-R1.fastq.gz &
+nohup cat `find . -name Tx16-1-*R2*fastq.gz` > temp/Tx16-1-R2.fastq.gz &
+
+nohup cat `find . -name Tx16-2-*R1*fastq.gz` > temp/Tx16-2-R1.fastq.gz &
+nohup cat `find . -name Tx16-2-*R2*fastq.gz` > temp/Tx16-2-R2.fastq.gz &
+
+nohup cat `find . -name Tx16-3-*R1*fastq.gz` > temp/Tx16-3-R1.fastq.gz &
+nohup cat `find . -name Tx16-3-*R2*fastq.gz` > temp/Tx16-3-R2.fastq.gz &
+
+nohup cat `find . -name Tx16-4-*R1*fastq.gz` > temp/Tx16-4-R1.fastq.gz &
+nohup cat `find . -name Tx16-4-*R2*fastq.gz` > temp/Tx16-4-R2.fastq.gz &
+
+nohup cat `find . -name Tx16-5-*R1*fastq.gz` > temp/Tx16-5-R1.fastq.gz &
+nohup cat `find . -name Tx16-5-*R2*fastq.gz` > temp/Tx16-5-R2.fastq.gz &
+
+nohup cat `find . -name Tx16-6-*R1*fastq.gz` > temp/Tx16-6-R1.fastq.gz &
+nohup cat `find . -name Tx16-6-*R2*fastq.gz` > temp/Tx16-6-R2.fastq.gz &
+
+nohup cat `find . -name Tx16-7-*R1*fastq.gz` > temp/Tx16-7-R1.fastq.gz &
+nohup cat `find . -name Tx16-7-*R2*fastq.gz` > temp/Tx16-7-R2.fastq.gz &
+
+nohup cat `find . -name Tx16-8-*R1*fastq.gz` > temp/Tx16-8-R1.fastq.gz &
+nohup cat `find . -name Tx16-8-*R2*fastq.gz` > temp/Tx16-8-R2.fastq.gz &
